742345052b13809a97d21ce4677b8934cb759936
xiaowu <2229048563@qq.com>
2024-04-30 23:16:16 +0800

    system call tracing

--- Makefile
+++ Makefile
@@ -193,7 +193,12 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
-
+	$U/_sleep\
+	$U/_find\
+	$U/_pingpong\
+	$U/_primes\
+	$U/_xargs\
+	$U/_trace\
 
 
 
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_trace(void) {
+  int mask;
+  if (argint(0, &mask) < 0) {
+    return -1;
+  }
+
+  struct proc* cur_proc = myproc();
+  cur_proc->trace_mask = mask;
+  return 0;
+}
